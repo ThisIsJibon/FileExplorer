e2050de0cd17284099228f41fcff7fe33544393e
Mehedi Hasan <jibonmh7@gmail.com>
2022-01-19 11:50:35 +0600

    UI initialized

---
+++ src/sample/HomeController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController extends Application implements Initializable {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
